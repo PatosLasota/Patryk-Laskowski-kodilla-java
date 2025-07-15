40c917acba66e61f6134ced0fb7a94bf1445a642
PatoS <patryk.laskowski.papug@gmail.com>
2025-07-15 23:36:58 +0200

    6.2 Added koddila-testing modle

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(1, 2);
+        int difference = calculator.subtract(1, 2);
+
+        if (sum == 3) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (difference == -1) {
+            System.out.println("test OK");
+        }  else {
+            System.out.println("Error!");
+        }
     }
 }
