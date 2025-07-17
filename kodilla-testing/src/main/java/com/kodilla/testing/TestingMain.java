bde7b0b740825da2bc9a140eef753e53bbce9df4
PatoS <patryk.laskowski.papug@gmail.com>
2025-07-17 23:39:21 +0200

    6.3 rror

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,31 +6,6 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
 
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-
-        int sum = calculator.add(1, 2);
-        int difference = calculator.subtract(1, 2);
-
-        if (sum == 3) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        if (difference == -1) {
-            System.out.println("test OK");
-        }  else {
-            System.out.println("Error!");
-        }
     }
 }
