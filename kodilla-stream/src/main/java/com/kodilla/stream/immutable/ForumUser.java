fa4b3e88504e47d56d3c409f9b7185b66d3c2a69
PatoS <patryk.laskowski.papug@gmail.com>
2025-08-04 19:41:10 +0200

    7.2. Czym jest Stream?

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,11 +1,11 @@
 package com.kodilla.stream.immutable;
 
 public final class ForumUser {
-    private String username;
-    private String realName;
+    final private String username;
+    final private String realName;
 
-    public ForumUser(String username, String realName) {
-        this.username = username;
+    public ForumUser(final String username, final String realName) {
+        this.username =  username;
         this.realName = realName;
     }
 
