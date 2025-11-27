537e5cce6283454b6b1422c4306074c0f99b579c
Laskowski <patryk.laskowski.papug@gmail.com>
2025-11-27 16:31:28 +0100

    12.1 Zadanie: funkcyjna iteracja po tytułach

---
+++ koddilla-good-patterns/src/main/java/com/kodilla/Main.java
@@ -0,0 +1,18 @@
+package com.kodilla;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        String result = movieStore.getMovies().values().stream()
+                .flatMap(List::stream)
+                .collect(Collectors.joining("!"));
+
+        System.out.println(result);
+    }
+}
