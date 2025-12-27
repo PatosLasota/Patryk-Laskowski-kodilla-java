34178e46aa94831fe3259e6c457421e9084b29d9
Laskowski <patryk.laskowski.papug@gmail.com>
2025-12-27 14:11:19 +0100

    Spring

---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.spring.intro.shape;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DrawerTestSuite {
+
+    @Test
+    void testDoDrawingWithCircle(){
+
+        Circle circle = new Circle();
+
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+
+        assertEquals("This is a circle figure", result);
+    }
+
+    @Test
+    void testDoDrawingWithTriangle(){
+
+        Triangle triangle = new Triangle();
+
+        Drawer drawer = new Drawer(triangle);
+        String result = drawer.doDrawing();
+
+        assertEquals("This is a triangle figure", result);
+    }
+}
